75e726c06f026a0caac01df5da7e3f3eec21b18b
unknown <parkan>
2025-12-12 10:28:41 +0700

    add car game

--- game-mobil/script.js
+++ game-mobil/script.js
@@ -11,27 +11,31 @@ car.src = 'image.png';
 
 
 let carX = 10;
-let carY = 120;
+let carY = 300;
+let speedObs = 5;
 
 let obstacle = new Image();
 obstacle.src = 'obstacle.png';
-// console.log(obstacle.src)
+console.log(canvas.width)
 let obs = [];
 obs[0] = {
     x: canvas.width,
     y: 110
 };
 
+let scoreElement = document.getElementById('score');
 let score = 0;
+scoreElement.innerHTML = score;
+
 
 window.addEventListener('keydown', (event) => {
-    if(event.key == "ArrowUp"){
-        if(carY >= 200) {
-            carY -=200;
+    if (event.key == "ArrowUp") {
+        if (carY >= 200) {
+            carY -= 200;
         }
-    } if(event.key == "ArrowDown"){
-        if(carY < 550){
-            carY +=200;
+    } if (event.key == "ArrowDown") {
+        if (carY < 550) {
+            carY += 200;
         }
     }
 
@@ -40,20 +44,57 @@ const main = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(car, carX, carY, 300, 200);
 
-    for(let i = 0; i < obs.length; i++){
-        ctx.drawImage(obstacle, obs[i].x, obs[i].y);
-        obs[i].x-=5;
+    // setInterval(() => {
+    //     speedObs +=5;
+    // }, 5000)
 
-        if(obs[i].x == 245) {
+    for (let i = 0; i < obs.length; i++) {
+        //  console.log(obs[i])
+        ctx.drawImage(obstacle, obs[i].x, obs[i].y, 300, 300);
+
+        obs[i].x -= speedObs;
+
+        if (obs[i].x == 12) {
+            score++;
+            scoreElement.innerHTML = score;
+        }
+
+        if (obs[i].x == 402) {
             obs.push({
                 x: canvas.width,
-                y: Math.random() * (canvas.height - obstacle.height)
+                y: Math.random() * (canvas.height - obstacle.height + 100)
 
             })
         }
+        if (
+            obs[i].x > 0 &&
+            carX + 200 > obs[i].x &&
+            carY < obs[i].y + 300 &&
+            carY  > obs[i].y) {
+                alert('anda kalah')
+                location.reload();
+                cancelAnimationFrame(animationFrame)
+            
+
+        }
     }
-    requestAnimationFrame(main)
+    let animationFrame = requestAnimationFrame(main)
+
+
 }
+
+// window.addEventListener('keydown', (e) => {
+//     main()
+// })
 main()
+
+window.addEventListener('click', (e) => {
+    // console.log('angka random: ' + Math.random() * (canvas.height - obstacle.height))
+    // console.log('koordinat y: ' + e.y);
+    // console.log('koordinat x: ' + e.x);
+    
+    console.log(carY)
+
+})
 
 
